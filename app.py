f53fe675ea1eaf34de314d24c713ccfac437925e
tanayr03 <tanayr03@gmail.com>
2017-05-24 19:53:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,48 @@
-from github import Github
-from github import UnknownObjectException
+import sys
 from flask import Flask
 from github import RateLimitExceededException
-import sys
+from github import Github
+from github import UnknownObjectException
+
 
 app = Flask(__name__)
-command_line_arguement = sys.argv[1].split("/")
+repo_url = sys.argv[1].split("/")
 
-for n in range(len(command_line_arguement)):
-    if "github" in command_line_arguement[n]:
-        username = command_line_arguement[n+1]
-        repository = command_line_arguement[n + 2]
+
+for x in range(len(repo_url)):
+    if "github" in repo_url[x]:
+        Git_usr = repo_url[x+1]
+        Git_repo = repo_url[x+2]
         break
 
-if n==len(command_line_arguement):
-    git_hub = "invalid url"
-
 try:
-    git_hub = Github().get_user(username).get_repo(repository)
+    git_username_repo = Github().get_user(Git_usr).get_repo(Git_repo)
 except UnknownObjectException:
-    git_hub = "repository does not exist. Please check link supplied"
-except Exception as e:
-    git_hub = "err : %s" % e
+    git_username_repo = "Invalid repository"
+except RateLimitExceededException:
+    git_username_repo = "Rate limit Has reached"
 
 
-def response(file_configuration):
+
+
+def Response_error_check(configuration):
     try:
-        return git_hub.get_file_contents(file_configuration).content.decode(git_hub.get_contents(file_configuration).encoding)
+        return git_username_repo.get_file_contents(configuration).content.decode(git_username_repo.get_contents(configuration).encoding)
     except UnknownObjectException as e:
-        return "exception : File not found"
+        return "File Not Found"
     except RateLimitExceededException:
-        return "exception : git rate limit reached"
-    except Exception as e:
-        return "exception occured: %s" % e
+        return "Rate Limit Exception"
 
-@app.route("/v1/<config_file>")
-def controller(config_file):
-    if isinstance(git_hub, basestring):
-        return git_hub
-    else:
-        return response(config_file)
+
+
+
+@app.route("/v1/<configuration>")
+def controller(configuration):
+        if isinstance(git_username_repo, basestring):
+            return git_username_repo
+        else:
+            return Response_error_check(configuration)
 
 if __name__ == "__main__":
     app.config["url"] = sys.argv[1]
-app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
